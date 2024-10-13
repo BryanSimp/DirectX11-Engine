fefab0d538c57fb24e5a6b6b1f89187a2d4321b6
Bryan Simpson <simpsonbm04@outlook.com>
2024-10-12 23:23:57 -0500

    Reorganized graphics.cpp to split up the functions to multiple header and cpp files. In bindable added protected to allow the children access to Graphics to use them to give them access to what they need. Then in every single file you can bind them for every single operation that each function needs to do.

--- hw3d/dxerr.h
+++ hw3d/dxerr.h
@@ -1,3 +1,16 @@
+//--------------------------------------------------------------------------------------
+// File: DXErr.h
+//
+// DirectX Error Library
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+
 #pragma once
 #include "ChiliWin.h"
 #include <sal.h>
